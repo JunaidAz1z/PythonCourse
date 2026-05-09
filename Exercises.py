36d79edb88cdb54c9c22e6f5db46657a42df61a8
Junaid Aziz <junaidaziz841@gmail.com>
2026-05-10 00:31:21 +0500

    Exercises for Practice

---
+++ Exercises.py
@@ -0,0 +1,64 @@
+
+                            #Data Types and Variables
+##Ex1
+# name = "Hello"
+# age = 2
+# city = "abcd"
+# print(name)
+# print(age)
+# print(city)
+
+# #Ex2
+# a = 14
+# b = 7.5
+# name = "Peter"
+# result1 = a + b
+# print(result1)
+# result2 = a * b
+# print(result2)
+# print(type(name))
+# print(type(a))
+
+# #Ex3
+# name = input("What is your name : ")
+# age = int(input("What is your age : "))
+# color = input("What is your Favourite color : ")
+
+# print(f"Hello {name}! Apki age {age} hai Apka favorite color {color} hai Aap agle saal {age+1} saal ke ho jayenge.")
+
+
+
+
+                                    #Strings
+
+#Ex1
+
+# name = "Kim Peter"
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(len(name))
+# print(name.replace("Peter", "Ahmed"))
+
+#Ex2
+
+# name = input("What is your name : ")
+# city = input("What is your city : ")
+# food = input("What is your fav food : ")
+# print(f"Hello {name} app {city} sy hain aur apka favorite food {food} hai.")
+
+#Ex3
+
+# string = "Programing"
+# print(string[:3])
+# print(string[3:6])
+# print(string[6:10])
+# print(string[::-1])
+
+#Ex4
+
+sentence = input("Enter a sentence :")
+word =sentence.split()
+print(len(word))
+print(sentence.upper())
+print(sentence.count("python"))
