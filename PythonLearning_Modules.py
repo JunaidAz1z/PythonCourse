8bdc6f8f6c5d58d58d227913bdb1f34d083f93dd
Junaid Aziz <junaidaziz841@gmail.com>
2026-05-09 13:25:40 +0500

    Function Caching

--- PythonLearning_Modules.py
+++ PythonLearning_Modules.py
@@ -28,13 +28,13 @@ import time
 # formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", t)
 # print(formatted_time)
 
-a = True
-print(a:=False)
+# a = True
+# print(a:=False)
 
-number = [1, 2, 3, 4, 5]
-while (n := len(number)) > 0:
-    print(f"{n} Elements in list")
-    print(number.pop())
+# number = [1, 2, 3, 4, 5]
+# while (n := len(number)) > 0:
+#     print(f"{n} Elements in list")
+#     print(number.pop())
 
 
 # food = list()
@@ -86,3 +86,113 @@ import os
 
 # response = requests.post(url, headers=headers, json=data)
 # print(response.json())
+
+
+
+#8cb8e0c2ddc5442282db0cae2537962f API KEY
+
+# import requests
+# import json
+# api_key = "8cb8e0c2ddc5442282db0cae2537962f"
+# query = input("Which News you want to see!")
+# response = requests.get(f"https://newsapi.org/v2/top-headlines?q={query}&apiKey={api_key}")
+
+# data = response.json()
+# for article in data["articles"]:
+#     print(article["title"])
+#     print(article["description"])
+
+# def my_generator():
+#     for i in range(50000):
+#         yield i
+
+# gen = my_generator()
+# print(next(gen))
+# print(next(gen))
+
+# for j in gen:
+#     print(j)
+
+                                #Generator Exercises
+
+#Ex1
+
+def my_gen():
+    for i in range(1,6):
+        yield i
+
+gen = my_gen()
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+
+
+#Ex2
+
+names = ["ali","tom","jhon"]
+def name_gen():
+    for name in names:
+        yield name
+n = name_gen()
+for j in n:
+    print(j)
+
+#Ex3
+
+def even_gen():
+    for i in range(1,10):
+        if (i%2==0):
+            yield i
+e = even_gen()
+for j in e:
+    print(j)
+
+#Ex4
+
+def table_gen():
+    for i in range(1,11):
+        yield(f"2 x {i} = {2 * i}")
+        
+tab = table_gen()
+for j in tab:
+    print(j)
+
+#Ex5
+
+string = "Hello World"
+def ch_gen():
+    for ch in string:
+        yield ch
+st = ch_gen()
+for j in st:
+    print(j)
+
+from functools import lru_cache
+import time
+
+@lru_cache(maxsize=None)
+
+def fun(n):
+    time.sleep(3)
+    return n*5
+
+a = fun(5)
+print(a)
+a = fun(6)
+print(a)
+a = fun(7)
+print(a)
+a = fun(5)
+print(a)
+a = fun(6)
+print(a)
+a = fun(7)
+print(a)
+a = fun(8)
+print(a)
+
+
+
+
